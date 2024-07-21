656157ce4c905202c11cfef8feafa1ddbcc8ee40
sh139 <853475219@qq.com>
2024-07-21 23:18:46 +0800

    修改登录网站的欢迎语，问答前需要登录

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -8,12 +8,13 @@ const cn = {
   Error: {
     Unauthorized: isApp
       ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确。"
-      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页填入你自己的 OpenAI API Key。",
+      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码",
+    // "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页填入你自己的 OpenAI API Key。",
   },
   Auth: {
-    Title: "需要密码",
-    Tips: "管理员开启了密码验证，请在下方填入访问码",
-    SubTips: "或者输入你的 OpenAI 或 Google API 密钥",
+    Title: "关注公众号，输入数字 1，获取访问码！输入数字2，办理会员；",
+    Tips: "普通用户，请在下方填入访问码；",
+    SubTips: "会员用户，请输入你的 OpenAI 密钥",
     Input: "在此处填写访问码",
     Confirm: "确认",
     Later: "稍后再说",
